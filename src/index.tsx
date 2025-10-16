66e6b58edcb3bf6299ff96e19034decb80359013
Alessio Margelli <alex9446@gmail.com>
2025-10-16 14:49:20 +0200

    moved pages and set sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 /* @refresh reload */
+import type { ParentComponent } from 'solid-js'
 import { render } from 'solid-js/web'
 import { Route, Router } from '@solidjs/router'
-import { Home } from './Home'
-import { Login } from './Login'
-import { Register } from './Register'
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import './index.sass'
 
 const root = document.getElementById('root')
 
+const Layout: ParentComponent = (props) => <main>{props.children}</main>
+
 render(
   () => (
-    <Router>
+    <Router root={Layout}>
       <Route path='/' component={Home} />
       <Route path='/login' component={Login} />
       <Route path='/register' component={Register} />
